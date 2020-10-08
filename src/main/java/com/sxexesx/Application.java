f8a50c34d98495cf8e0cedfdcba3d39f8cd3cce6
sxexesx <>
2020-10-08 23:37:09 +0300

    initial commit

---
+++ src/main/java/com/sxexesx/Application.java
@@ -0,0 +1,11 @@
+package com.sxexesx;
+
+import com.sxexesx.service.DroolsEngine;
+
+public class Application {
+
+    public static void main(String[] args) {
+        DroolsEngine droolsEngine = new DroolsEngine();
+        droolsEngine.startCalculation();
+    }
+}
---
+++ src/main/java/com/sxexesx/models/Store.java
@@ -0,0 +1,4 @@
+package com.sxexesx.models;
+
+public class Store {
+}
